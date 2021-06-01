635384a986ddfb7fb432c09d525db3a51793c915
miueon <cruxbell@gmail.com>
2021-06-01 21:40:58 +0800

    my solution for all exercise in lab syscall

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,21 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+uint64
+free_mem(void)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  uint64 count = 0;
+  while (r)
+  {
+    r = r->next;
+    count++;
+  }
+  release(&kmem.lock); 
+  
+  return count * 4096;
+}
--- kernel/proc.c
+++ kernel/proc.c
@@ -295,6 +295,8 @@ fork(void)
 
   np->state = RUNNABLE;
 
+  np->trace_mask = p->trace_mask;
+
   release(&np->lock);
 
   return pid;
@@ -692,4 +694,19 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+uint64
+used_proc(void)
+{
+  struct proc *p;
+  uint64 count = 0;
+  for ( p = proc; p < &proc[NPROC]; p++)
+  {
+    if (p->state != UNUSED)
+    {
+      count++; 
+    }
+  }
+  return count;
 }
